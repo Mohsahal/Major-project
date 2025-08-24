fa951ad85da5d8ae53816cd19c759b087c221fa7
Mohsahal <mohammedsahal1243@gmail.com>
2025-08-24 20:49:13 +0530

    Update database.ts

--- client/src/config/database.ts
+++ client/src/config/database.ts
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = (import.meta as any)?.env?.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = (import.meta as any)?.env?.VITE_API_BASE_URL || 'https://job-reco-backend.onrender.com/api';
 
 export const db = {
   collection: (collectionName: string) => ({
